68ec215c16c0f73df2e4ce7e8c1f7b8871cf3728
itggot-gosta-larsson <gosta.larsson@itggot.se>
2018-04-07 04:25:32 +0200

    Setup routes for login, register and contacts

--- app.rb
+++ app.rb
@@ -1,7 +1,25 @@
 class App < Sinatra::Base
 
     get '/' do
-        ":)"
+        redirect '/login'
+    end
+
+    get '/login' do
+        slim :login
+    end
+    post '/login' do
+        
+    end
+
+    get '/register' do
+        slim :register
+    end
+    post '/register' do
+
+    end
+
+    get '/contacts' do
+        slim :contacts
     end
 
 end

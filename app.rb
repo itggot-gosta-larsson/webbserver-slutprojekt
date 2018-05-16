594e35837647b0271b8cdd1079e085181b2c0b7e
itggot-gosta-larsson <gosta.larsson@itggot.se>
2018-05-16 23:39:51 +0200

    Added basic socialgroups and contacts

--- app.rb
+++ app.rb
@@ -61,10 +61,89 @@ class App < Sinatra::Base
         slim :register
     end
 
+    #socialgroups
+    get '/socialgroups' do
+        if !@user
+            redirect '/login'
+        end
+        @socialgroups = @database.execute('SELECT * FROM socialgroups WHERE user_id IS ?', @user[0])
+        slim :socialgroups
+    end
+    post '/socialgroups/add' do
+        if !@user
+            redirect '/login'
+        end
+        result = @database.execute('SELECT * FROM socialgroups WHERE user_id IS ? AND name IS ?', @user[0], params['name'])
+        if result.first == nil
+            @database.execute('INSERT INTO socialgroups(user_id, name) VALUES (?,?)', @user[0], params['name'])
+        end
+        redirect '/socialgroups'
+    end
+    post '/socialgroups/delete/:id' do
+        if !@user
+            redirect '/login'
+        end
+        result = @database.execute('SELECT * FROM socialgroups WHERE user_id IS ? AND id IS ?', @user[0], params['id'])
+        if result.first != nil
+            @database.execute('DELETE FROM socialgroups WHERE id IS ?', params['id'])
+        end
+        redirect '/socialgroups'
+    end
+
+    #contacts
+
+    get '/contacts/add' do
+        if !@user
+            redirect '/login'
+        end
+
+        @socialgroups = @database.execute('SELECT * FROM socialgroups WHERE user_id IS ?', @user[0])
+
+        slim :'contacts/add'
+    end
+    post '/contacts/add' do
+        if !@user
+            redirect '/login'
+        end
+
+        @database.execute('INSERT INTO contacts(user_id, name, email, number) VALUES (?, ?, ?, ?)', @user[0], params['name'], params['email'], params['number'])
+        socialgroups = []
+        params.each do |s,v|
+            if s.start_with? "socialgroup"
+                n = s.split("_")[1].to_i
+                socialgroups += [n]
+            end
+        end
+        test_statement = ""
+        socialgroups.each do |s|
+            if test_statement != ""
+                test_statement += " OR "                
+            end
+            test_statement += "id IS #{s}"
+        end
+        confirmed_socialgroups = @database.execute('SELECT * FROM socialgroups WHERE user_id IS ? AND (?)', @user[0], test_statement)
+        puts "SELECT * FROM socialgroups WHERE user_id IS #{@user[0]} AND (#{test_statement})"
+        puts confirmed_socialgroups
+        redirect '/contacts'
+    end
+
+    get '/contacts/:id' do
+        if !@user
+            redirect '/login'
+        end
+
+        @contact_id = params[:id]
+
+        slim :'contacts/view'
+    end
+
     get '/contacts' do
         if !@user
             redirect '/login'
         end
+
+        @contacts = @database.execute('SELECT * FROM contacts WHERE user_id IS ?', @user[0])
+
         slim :contacts
     end
 
